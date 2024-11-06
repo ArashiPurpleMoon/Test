7b27af1415e0160b955e4e26be8a1bb96749f485
ArashiPurpleMoon <chengweizhong0614@gmail.com>
2024-11-06 12:05:25 +0800

    修改bug

--- Student.java
+++ Student.java
@@ -1,6 +1,9 @@
 public class Student extends StudentModel {
 	
+	String name = getName();
+	Integer age = getAge();
+	
 	void introduce() {
-		System.out.println("I'm " + name + ", " + age + " years old.");
+		System.out.println("I'm " + name + ", " + String.valueOf(age) + " years old.");
 	}
 }
--- Teacher.java
+++ Teacher.java
@@ -1,6 +1,10 @@
 public class Teacher extends TeacherModel {
 	
+	String name = getName();
+	Integer age = getAge();
+	String subject = getSubject();
+	
 	void introduce() {
-		System.out.println("I'm " + name + ", " + age + " years old, teaching subject is " + subject + ".");
+		System.out.println("I'm " + name + ", " + String.valueOf(age) + " years old, teaching subject is " + subject + ".");
 	}
 }
